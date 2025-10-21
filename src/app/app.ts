927c213643d99271f2d8d78bac7a211e68710194
Ben Abbott <B.Abbott@autoflow.ltd.uk>
2025-10-21 11:32:03 +0100

    Add form validation to title

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
+import {FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import IFilm from './models/IFilm'
 import defaultFilms from './data/films'
 
@@ -12,16 +12,40 @@ import defaultFilms from './data/films'
 })
 export class App {
   
+  formFailedSubmit = false;
+
   filmForm = new FormGroup({
-    title: new FormControl(''),
+    title: new FormControl('', Validators.required),
     releaseYear: new FormControl(''),
     director: new FormControl(''),
     rating: new FormControl(''),  
   });
 
+  get titleFormControl() {
+    return this.filmForm.get('title');
+  }
+
+  get releaseYearFormControl() {
+    return this.filmForm.get('releaseYear');
+  }
+
+  get directorFormControl() {
+    return this.filmForm.get('director');
+  }
+
+  get ratingFormControl() {
+    return this.filmForm.get('rating');
+  }
+
   films = signal<IFilm[]>(defaultFilms);
 
   addFilm() {
+
+    if(this.titleFormControl?.invalid || this.releaseYearFormControl?.invalid || this.directorFormControl?.invalid || this.ratingFormControl?.invalid) {
+      this.formFailedSubmit = true;
+      return;
+    };
+
     const newFilm: IFilm = {
       id: this.generateFilmId(),
       title: this.filmForm.value.title || '',
@@ -32,20 +56,13 @@ export class App {
 
     this.films.update(films => [...films, newFilm]);
 
-    this.resetForm();
+    this.filmForm.reset();
+
+    this.formFailedSubmit = false;
   }
 
   generateFilmId() {
     const currentIds = this.films().map(e => e.id);
     return Math.max(...currentIds) + 1;
-  }
-
-  resetForm() {
-    this.filmForm.setValue(    {
-      title: '',
-      releaseYear: '',
-      director: '',
-      rating: '5'  
-    },);
   }
 }
